18be244abb91e465763721fc75f0f3be3a0d406e
Emilia Nemes <nemes.emilia99@gmail.com>
2021-08-09 19:28:22 +0300

    testing heroku v10

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,9 @@ class ApplicationController < ActionController::Base
     def after_sign_in_path_for(resource)
         stored_location_for(resource) || home_dashboard_path
     end
+
+    def current_user
+        token = request.headers["Authorization"].to_s
+        User.find_for_database_athentication(authentication_token: token)
+    end
 end
