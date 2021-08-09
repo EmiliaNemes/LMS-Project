a71b8842fcfab5282ca2056d5fa4dc077884cac1
Emilia Nemes <nemes.emilia99@gmail.com>
2021-08-09 18:36:30 +0300

    testing heroku v3

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,13 @@
 class HomeController < ApplicationController
   before_action :set_current_user
+  before_action :set_current_school
 
   def set_current_user
     @current_user = current_user
+  end
+
+  def set_current_school
+    @current_school = current_user.school.name
   end
 
   def index
